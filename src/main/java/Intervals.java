9fe3d69e026a7b2231498b919bba6c6bd18d0344
Eduard Klygunov <eduard.klygunov@gmail.com>
2023-03-12 12:26:01 +0300

    init

---
+++ src/main/java/Intervals.java
@@ -0,0 +1,34 @@
+public class Intervals {
+
+    public static String intervalConstruction(String[] args) {
+        return null;
+    }
+
+    public static String intervalIdentification(String[] args) {
+        return null;
+    }
+
+    private static String getDegreeOfNote(String note) {
+        return null;
+    }
+
+    private static String getSemitonesOfNote(String note) {
+        return null;
+    }
+
+    private static String getDegreeOfInterval(String interval) {
+        return null;
+    }
+
+    private static String getSemitonesOfInterval(String interval) {
+        return null;
+    }
+
+    private static String getNoteFromDegreeAndSemitones(String degree, String semitones) {
+        return null;
+    }
+
+    private static String getIntervalFromDegreeAndSemitones(String degree, String semitones) {
+        return null;
+    }
+}
